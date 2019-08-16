3aeb9295ac170e77192d88e3d3602bd08d55b961
Bathandwa Lolwana <mr.lolwana@gmail.com>
2019-08-16 22:11:42 +0200

    Remove edges that overlap ostacles.

--- Part_I/src/Graph/PRM.java
+++ Part_I/src/Graph/PRM.java
@@ -18,7 +18,7 @@ import algos.Search;
 
 public class PRM {
 
-    public static void generate() {
+    public static double[][] generate() {
         Scanner in = new Scanner(System.in);
         /*
          - read first line of input; convert each to integer
@@ -138,15 +138,18 @@ public class PRM {
             }
         }
 
+        // remove edges that overlap obstacles
+        removeEdges(adj, samples, top_left, bottom_right);
+
         for (double[] row: adj) {
-            // System.out.println( Arrays.toString( row ) );
-            for ( double i: row ) {
-                System.out.print( (int) i + " " );
-            }
-            System.out.println("\n");
+            String x = Arrays.toString( row );
+            String y = x.replace(".0", "");
+            System.out.println( y );
         }
 
         in.close();
+
+        return adj;
     }
 
     // helper function for generate; removes samples that removes samples that overlap obstacles
@@ -167,6 +170,122 @@ public class PRM {
                     }
                 }
 
+            }
+        }
+    }
+
+    /*
+       - Check if the lines between two nodes crosses any of the 
+       boundary lines of each obstacle. If it does. remove the
+       edge between them.
+       - Represent the line as a function of x [line_0] and evaluate it at
+       each boundary x-value. Remove an edge if the output value
+       of the line is in between the boundary y-values of
+       the obstacle.
+       - Represent the line as a function of y [line_1] and evaluate it at
+       each boundary y-value. Remove an edge if the output value
+       of the line is in between the boundary x-values of
+       the obstacle.
+    */
+    public static void removeEdges(double[][] adj, ArrayList<Point> nodes, ArrayList<Point> top_left,
+            ArrayList<Point> bottom_right) {
+        int n_nodes = adj.length, n_obst = top_left.size();
+        for ( int i = 0; i < n_nodes; ++i ) {
+            for ( int j = i + 1; j < n_nodes; ++j ) {
+                if ( adj[i][j] == 0 ) {
+                    continue;
+                }
+
+                // get the nodes that make the adjacency
+                Point a = nodes.get(i);
+                Point b = nodes.get(j);
+
+                // top-left and bottom-right corners of the rectangle formed by nodes a and b
+                Point edge_tl = new Point( Math.min( a.x, b.x ), Math.min( a.y, b.y ) );
+                Point edge_br = new Point( Math.max( a.x, b.x ), Math.max( a.y, b.y ) );
+
+
+                /*
+                 - if y values are equal, check that the value is within the horizontal boundaries of
+                   the obstacle and that the x values are outside of the vertical boundaries of the
+                   obstacle.
+                 - if x values are equal, check that the value is within the vertical boundaries of
+                   the obstacle and that the y values are outside of the horizontal boundaries of the
+                   obstacle.
+                */
+                if ( a.y == b.y ) {
+                    for ( int k = 0; k < n_obst; ++k ) {
+                        if ((top_left.get(k).y <= a.y && a.y <= bottom_right.get(k).y)
+                                && (a.x <= top_left.get(k).x && bottom_right.get(k).x <= a.x)) {
+                            adj[i][j] = 0;
+                            adj[j][i] = 0;
+                            break;
+                        }
+                    }
+                } else if ( a.x == b.x ) {
+                    for ( int k = 0; k < n_obst; ++k ) {
+                        if ((top_left.get(k).x <= a.x && a.x <= bottom_right.get(k).x)
+                                && (a.y <= top_left.get(k).y && bottom_right.get(k).y <= a.y)) {
+                            adj[i][j] = 0;
+                            adj[j][i] = 0;
+                            break;
+                        }
+                    }
+                } else {
+                    // slope of the line_0
+                    double m_y = (double) (b.y - a.y) / (b.x - a.x);
+                    // slope of the line_1
+                    double m_x = (double) (b.x - a.x) / (b.y - a.y);
+    
+                    // y intercept of the line_0
+                    double c_y = b.y - b.x * m_y;
+                    // x intercept of the line_1
+                    double c_x = b.x - b.y * m_x;
+    
+                    /*
+                       for every obstacle, check if the output of each line at each boundary value
+                       is within the relevant boundary range (i.e. y-range of obstacle for line_0
+                       and x-range of obstacle for line_1)
+                    */
+                    for ( int k = 0; k < n_obst; ++k ) {
+                        double x_left = top_left.get(k).x,
+                               x_right = bottom_right.get(k).x,
+                               y_top = top_left.get(k).y,
+                               y_bottom = bottom_right.get(k).y;
+
+                        /*
+                           The obstacle must intersect the rectangle created by the nodes. If this isn't the
+                           case, then the line through the points may intersect the obstacle elsewhere i.e.
+                           another part of the line which isn't the edge between the nodes overlaps the ob-
+                           stacle.
+                        */
+                        if ((edge_tl.y <= y_top && y_top <= edge_br.y)
+                                || (edge_tl.y <= y_bottom && y_bottom <= edge_br.y)
+                                || (edge_tl.x <= x_left && x_left <= edge_br.x)
+                                || (edge_tl.x <= x_right && x_right <= edge_br.x)) {
+                                    
+                            // evaluate line_0 at boundary x-values
+                            double y_0 = m_y * x_left + c_y;
+                            double y_1 = m_y * x_right + c_y;
+        
+                            // evaluate line_1 at boundary y-values
+                            double x_0 = m_x * y_top + c_x;
+                            double x_1 = m_x * y_bottom + c_x;
+        
+        
+                            if ((y_top <= y_0 && y_0 <= y_bottom)
+                                    || (y_top <= y_1 && y_1 <= y_bottom)
+                                    || (x_left <= x_0 && x_0 <= x_right)
+                                    || (x_left <= x_1 && x_1 <= x_right)) {
+    
+                                adj[i][j] = 0;
+                                adj[j][i] = 0;
+                                break;
+                            }
+
+                        }
+                    }
+                }
             }
         }
     }
